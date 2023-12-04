b3d70000b7f1f33dfd845176f52846b8716fed3d
easy-0 <lc_dyna@naver.com>
2023-12-04 17:26:43 +0900

    231204 JAVA, Servlet&JSP, Spring

--- 3.스프링/spring_study/exam01/src/main/java/models/Greeter.java
+++ 3.스프링/spring_study/exam01/src/main/java/models/Greeter.java
@@ -2,6 +2,6 @@ package models;
 
 public class Greeter {
     public void hello(String name){
-        System.out.printf("%s님 안녕하세요!%n", name);
+        System.out.printf("%s님 안녕하세요!!%n", name);
     }
 }
